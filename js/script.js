544ddb5c0b180c83b975915aa9c6d53f9d224bb3
Razvan <razvanrus289@yahoo.com>
2019-04-15 17:26:36 -0500

    edited the js based on the new example

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42.047778, lng: -87.756829},
     zoom: 10
   });
