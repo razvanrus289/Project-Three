412afd39554b9081cc5fd99e08e757506f1ecf64
Razvan <razvanrus289@yahoo.com>
2019-04-22 12:33:17 -0500

    I edited the slider code to make it look cleaner and have the arrows be below the pictures

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.047778, lng: -87.756829},
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 42.047778, lng: -87.756829 },
     zoom: 15
   });
 }
-
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-function showDivs(n) {
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  x[slideIndex-1].style.display = "block";
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
